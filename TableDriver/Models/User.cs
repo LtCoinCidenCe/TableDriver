2faa6a9ab20a5a8cc15943c9fc6f9addb8dd9231
Kizuna Akari <lecion20@outlook.com>
2025-01-14 23:59:50 +0800

    again before migri

--- TableDriver/Models/User.cs
+++ TableDriver/Models/User.cs
@@ -5,32 +5,7 @@ using TableDriver.Models.Misc;
 
 namespace TableDriver.Models
 {
-    // This dictates the database columns
-    [Index(nameof(Username),IsUnique =true)]
-    [Index("DisplayName")]
-    public class User
+    public class User: UserAbstract
     {
-        public ulong ID { get; set; }
-
-        [MaxLength(57)]
-        [RegularExpression("^[A-Za-z][A-Za-z0-9]{4,55}$")]
-        public string Username { get; set; } = string.Empty;
-
-        [MaxLength(60)]
-        public string DisplayName { get; set; } = string.Empty;
-
-        [MaxLength(32)]
-        public byte[] Passhash { get; set; } = new byte[32];
-
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        public DateTime CreatedAt { get; set; }
-
-        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
-        public DateTime LastUpdatedAt { get; set; }
-
-        [MaxLength(120)]
-        public string Introduction { get; set; } = string.Empty;
-
-        public Gender Gender { get; set; } = Gender.secret;
     }
 }
--- TableDriver/Models/UserMemory.cs
+++ TableDriver/Models/UserMemory.cs
@@ -5,31 +5,7 @@ using TableDriver.Models.Misc;
 
 namespace TableDriver.Models
 {
-    [Index(nameof(Username), IsUnique = true)]
-    [Index("DisplayName")]
-    public class UserMemory
+    public class UserMemory : UserAbstract
     {
-        public ulong ID { get; set; }
-
-        [MaxLength(57)]
-        [RegularExpression("^[A-Za-z][A-Za-z0-9]{4,55}$")]
-        public string Username { get; set; } = string.Empty;
-
-        [MaxLength(60)]
-        public string DisplayName { get; set; } = string.Empty;
-
-        [MaxLength(32)]
-        public byte[] Passhash { get; set; } = new byte[32];
-
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        public DateTime CreatedAt { get; set; }
-
-        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
-        public DateTime LastUpdatedAt { get; set; }
-
-        [MaxLength(120)]
-        public string Introduction { get; set; } = string.Empty;
-
-        public Gender Gender { get; set; } = Gender.secret;
     }
 }
