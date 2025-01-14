84ca47aea2d405cccda4161a999a08bf046dc1af
Kizuna Akari <lecion20@outlook.com>
2025-01-14 20:44:38 +0800

    good prepared

--- TableDriver/Controllers/UserController.cs
+++ TableDriver/Controllers/UserController.cs
@@ -1,21 +1,55 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+using System.Security.Cryptography;
+using System.Text;
 using TableDriver.Models;
+using TableDriver.Models.Misc;
+using TableDriver.Services;
 
 namespace TableDriver.Controllers;
 
 [ApiController]
 [Route("[controller]")]
-public class UserController(ILogger<UserController> logger) : ControllerBase
+public class UserController(ILogger<UserController> logger, UserService userService) : ControllerBase
 {
     [HttpGet]
     public IEnumerable<User> GetAllUsers()
     {
-        return new List<User>() { new User() };
+        return userService.AllUsers();
     }
 
     [HttpPost]
-    public IActionResult CreateNewUser()
+    public User CreateNewUser(UserNew newUser)
     {
-        return Ok();
+        User entity = new()
+        {
+            Username = newUser.Username,
+            DisplayName = newUser.DisplayName,
+            Introduction = newUser.Introduction,
+            Passhash = SHA256.HashData(Encoding.UTF8.GetBytes(newUser.Password + "SuperSalt")),
+            Gender = newUser.Gender,
+        };
+        User result = userService.CreateNewUser(entity);
+        return result;
     }
+}
+
+public class UserNew
+{
+    [MinLength(3)]
+    [RegularExpression("^[A-Za-z][A-Za-z0-9]{4,55}$")]
+    public string Username { get; set; } = string.Empty;
+
+    [MinLength(2)]
+    [MaxLength(60)]
+    public string DisplayName { get; set; } = string.Empty;
+
+    [MinLength(3)]
+    [MaxLength(100)]
+    public string Password { get; set; } = string.Empty;
+
+    [MaxLength(120)]
+    public string Introduction { get; set; } = string.Empty;
+
+    public Gender Gender { get; set; } = Gender.secret;
 }
---
+++ TableDriver/Services/UserService.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TableDriver.DBContexts;
+using TableDriver.Models;
+
+namespace TableDriver.Services;
+
+public class UserService(UserContext userContext)
+{
+    public List<User> AllUsers()
+    {
+        return userContext.User.AsNoTracking().ToList();
+    }
+
+    public User? GetUserbyID(ulong id)
+    {
+        return userContext.User.SingleOrDefault(u => u.ID == id);
+    }
+
+    public User CreateNewUser(User user)
+    {
+        userContext.User.Add(user);
+        userContext.SaveChanges();
+        return user;
+    }
+}
