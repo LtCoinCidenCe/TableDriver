667869ad0fdd92451265677baca1f752bbce3cd7
Kizuna Akari <lecion20@outlook.com>
2025-01-15 12:56:05 +0800

    Quan Dou You

--- TableDriver/Migrations/20250115045547_WoQuanDouYao.Designer.cs
+++ TableDriver/Migrations/20250115045547_WoQuanDouYao.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TableDriver.DBContexts;
 
@@ -11,9 +12,11 @@ using TableDriver.DBContexts;
 namespace TableDriver.Migrations
 {
     [DbContext(typeof(UserContext))]
-    partial class UserContextModelSnapshot : ModelSnapshot
+    [Migration("20250115045547_WoQuanDouYao")]
+    partial class WoQuanDouYao
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
