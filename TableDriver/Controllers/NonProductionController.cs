c317be4b5275890d48e50f16c92753291b3e3616
Kizuna Akari <lecion20@outlook.com>
2025-02-01 14:22:37 +0800

    Clear init database

---
+++ TableDriver/Controllers/NonProductionController.cs
@@ -0,0 +1,20 @@
+﻿#if DEBUG
+using Microsoft.AspNetCore.Mvc;
+using TableDriver.DBContexts;
+
+namespace TableDriver.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NonProductionController(UserContext userContext) : ControllerBase
+    {
+        [HttpDelete]
+        public ActionResult ClearInitDatabase()
+        {
+            userContext.Database.EnsureDeleted();
+            userContext.Database.EnsureCreated();
+            return NoContent();
+        }
+    }
+}
+#endif
