c24a4ffac130899266861d7f9a43eae0dd842539
CoinCidenCe <49898006+LtCoinCidenCe@users.noreply.github.com>
2025-05-10 16:32:16 +0000

    optimistic concurrency control and early validation

--- TableDriver/Services/UserService.cs
+++ TableDriver/Services/UserService.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.Reflection;
 using Microsoft.EntityFrameworkCore;
 using TableDriver.DBContexts;
 using TableDriver.Models.User;
@@ -94,35 +95,60 @@ public class UserService(UserContext userContext)
 
     public int ModifyDisplayName(string userid, string newDisplayName)
     {
-        User? uuu;
-        if (long.TryParse(userid, out var id))
+        for (int retries = 0; retries < 5; retries++)
         {
-            uuu = userContext.User.SingleOrDefault(user => user.ID == id);
-        }
-        else
-        {
-            uuu = userContext.User.SingleOrDefault(user => user.Username == userid);
-        }
-        if (uuu is null)
-        {
-            return 0;
-        }
-        var history = new UserDisplayNameHistory()
-        {
-            User = uuu,
-            LastUpdatedAt = uuu.LastUpdatedAt,
-            DisplayName = uuu.DisplayName
-        };
-        userContext.UserDisplayNameHistory.Add(history);
-        uuu.DisplayName = newDisplayName;
-        uuu.LastUpdatedAt = DateTime.UtcNow;
+            User doll = new() { DisplayName = newDisplayName };
+            ValidationContext validationContext = new(doll);
+            PropertyInfo? property = typeof(User).GetType().GetProperty("DisplayName");
+            if (property is null)
+                return 99;
+            var attrs = property.GetCustomAttributes(true).OfType<ValidationAttribute>();
+            var valid = Validator.TryValidateValue(newDisplayName, validationContext, null, attrs);
+            if (!valid)
+            {
+                return 2;
+            }
 
-        bool vvv = DBValidator.ValidateDBObject(uuu, out var invalids);
-        if (!vvv)
-        {
-            return 2;
+            User? uuu;
+            if (long.TryParse(userid, out var id))
+            {
+                uuu = userContext.User.AsNoTracking().SingleOrDefault(user => user.ID == id);
+            }
+            else
+            {
+                uuu = userContext.User.AsNoTracking().SingleOrDefault(user => user.Username == userid);
+            }
+            if (uuu is null)
+            {
+                return 0;
+            }
+
+            // optimistic concurrency control
+            int updates = userContext.User.Where(user => user.ID == id && user.LastUpdatedAt == uuu.LastUpdatedAt)
+            .ExecuteUpdate(setter => setter
+            .SetProperty(row => row.DisplayName, newDisplayName)
+            .SetProperty(row => row.LastUpdatedAt, DateTime.UtcNow)
+            );
+            if (updates == 0)
+            {
+                continue;
+            }
+            if (updates > 1)
+            {
+                return 99; // db system error
+            }
+
+            var history = new UserDisplayNameHistory()
+            {
+                // User = uuu, // uuu is out of date
+                UserID = uuu.ID,
+                LastUpdatedAt = uuu.LastUpdatedAt,
+                DisplayName = uuu.DisplayName
+            };
+            userContext.UserDisplayNameHistory.Add(history);
+            userContext.SaveChanges();
+            return 1; // correct return
         }
-        userContext.SaveChanges();
-        return 1;
+        return 5; // too many retries
     }
 }
